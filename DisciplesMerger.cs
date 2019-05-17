17fdaf58715bb600c54b6980c630e8a6ec732835
willomae <wilburomae@gmail.com>
2019-05-17 15:58:40 +0300

    created DisciplesMerger class

---
+++ DisciplesMerger.cs
@@ -0,0 +1,36 @@
+﻿using DisciplesMerger.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DisciplesMerger
+{
+    partial class Program
+    {
+        public class DisciplesMerger
+        {
+            private Database[] databases;
+            public DisciplesMerger(string[] filenames)
+            {
+                databases = new Database[filenames.Length];
+                for (int i = 0; i < filenames.Length; i++)
+                {
+                    databases[i] = new Database(filenames[i]);
+                }
+            }
+            public void Merge()
+            {
+                MergeAttendance();
+            }
+            private void MergeAttendance()
+            {
+                HashSet<Attendance> attendances = new HashSet<Attendance>();
+                for (int i = 0; i < databases.Length; i++)
+                {
+                    attendances.Union(Attendance.Read(this.databases[i]));
+                }
+                Console.WriteLine("count. " + attendances.Count);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,16 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DisciplesMerger
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test");
+            var NEWLINE = Environment.NewLine;
+            var DOUBLENEWLINE = NEWLINE + NEWLINE;
+            var launchPath = Environment.CurrentDirectory;
+            var databasesPath = Path.Combine(launchPath, "Databases");
+            var databaseFolder = Directory.CreateDirectory(databasesPath);
+            var files = databaseFolder.GetFiles();
+
+            Console.WriteLine("Enumerating files in " + databasesPath);
+            Console.WriteLine("Found {0} files as listed below:", files.Length);
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine("{0}. " + files[i], i + 1);
+            }
+            string response = string.Empty;
+            do
+            {
+                Console.WriteLine("Are they all databases? Yes(y) to proceed, No (n) to cancel.");
+                var input = Console.ReadLine();
+                response = input.ToLower().Contains('y') ? "yes" : input.ToLower().Contains('n') ? "exit" : "invalid";
+            } while (!response.Equals("yes") && !response.Equals("exit"));
+
+            switch (response)
+            {
+                case "yes":
+                    string[] filenames = new string[files.Length];
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        filenames[i] = files[i].FullName;
+                    }
+                    var disciplesMerger = new DisciplesMerger(filenames);
+                    disciplesMerger.Merge();
+                    break;
+                case "exit":
+                    break;
+            }
+            Console.ReadKey();
         }
     }
 }
