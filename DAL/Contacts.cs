01df4addf0c13e00b92426c07ab3f5423328c22d
willomae <wilburomae@gmail.com>
2019-05-17 12:20:20 +0300

    created contacts class

---
+++ DAL/Contacts.cs
@@ -0,0 +1,21 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Contacts
+    {
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string fk_names_guid { get; set; }
+        public string fk_workers_guid { get; set; }
+        public string date { get; set; }
+        public string duration { get; set; }
+        public string type { get; set; }
+        public string studytype { get; set; }
+        public string fk_studyseries_guid { get; set; }
+        public string studynumber { get; set; }
+        public string fk_studytopics_guid { get; set; }
+        public string studytopicother { get; set; }
+        public string completed { get; set; }
+        public string notes { get; set; }
+    }
+}
