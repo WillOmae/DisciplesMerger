ea547d2c9cdd3dd7af90ad746b0f3fd834ca1c79
willomae <wilburomae@gmail.com>
2019-05-17 11:37:29 +0300

    created sessions class

---
+++ DAL/Sessions.cs
@@ -0,0 +1,21 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Sessions
+    {
+        public int id { get; set; }
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string fk_events_guid { get; set; }
+        public string night { get; set; }
+        public string date { get; set; }
+        public string title { get; set; }
+        public string subject { get; set; }
+        public string venue { get; set; }
+        public string presenter { get; set; }
+        public string fk_workers_guid { get; set; }
+        public string fk_lessons_guid { get; set; }
+        public string materials { get; set; }
+        public string notes { get; set; }
+    }
+}
