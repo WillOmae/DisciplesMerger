3fe16e870332970f0d4c2ebb61529d260a332992
willomae <wilburomae@gmail.com>
2019-05-17 11:32:37 +0300

    created synclogs class

---
+++ DAL/Synclogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisciplesMerger.DAL
+{
+    public class Synclogs
+    {
+        public int id { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string tablename { get; set; }
+        public string fk_guid { get; set; }
+        public string action { get; set; }
+    }
+}
