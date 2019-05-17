d5a4021830c2cfa3ac24f43d76b8ecd7a711a061
willomae <wilburomae@gmail.com>
2019-05-17 11:39:23 +0300

    created relationships class

---
+++ DAL/Relationships.cs
@@ -0,0 +1,14 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Relationships
+    {
+        public int id { get; set; }
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string fk_names_guid { get; set; }
+        public string fk_names_guid2 { get; set; }
+        public string type { get; set; }
+        public string sort { get; set; }
+    }
+}
