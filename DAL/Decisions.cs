4c3da78380ce59c4ed65d40579a65776fb3b3841
willomae <wilburomae@gmail.com>
2019-05-17 12:19:05 +0300

    created decisions class

---
+++ DAL/Decisions.cs
@@ -0,0 +1,16 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Decisions
+    {
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string fk_names_guid { get; set; }
+        public string fk_registrations_guid { get; set; }
+        public string fk_contacts_guid { get; set; }
+        public string fk_sessions_guid { get; set; }
+        public string decision { get; set; }
+        public string decisionother { get; set; }
+        public string decisioncategory { get; set; }
+    }
+}
