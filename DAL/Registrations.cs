85bb24b53e0e01708e4d71928a1b6e44f8f7e7d4
willomae <wilburomae@gmail.com>
2019-05-17 11:42:42 +0300

    created registrations class

---
+++ DAL/Registrations.cs
@@ -0,0 +1,16 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Registrations
+    {
+        public string id { get; set; }
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string fk_names_guid { get; set; }
+        public string fk_events_guid { get; set; }
+        public string fk_workers_guid { get; set; }
+        public string ticketnumber { get; set; }
+        public string advertising { get; set; }
+        public string notes { get; set; }
+    }
+}
