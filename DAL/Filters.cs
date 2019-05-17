85f38d77a383de5fcd280e39528e6e30042d1e7d
willomae <wilburomae@gmail.com>
2019-05-17 12:16:49 +0300

    created filters class

---
+++ DAL/Filters.cs
@@ -0,0 +1,15 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Filters
+    {
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_workers_guid { get; set; }
+        public string name { get; set; }
+        public string andor { get; set; }
+        public string categories { get; set; }
+        public string columns { get; set; }
+        public string operators { get; set; }
+        public string queries { get; set; }
+    }
+}
