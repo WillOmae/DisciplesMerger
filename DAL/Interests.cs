eb1c08d799f2405505e07d88ef50ed34ee8e05b8
willomae <wilburomae@gmail.com>
2019-05-17 11:59:59 +0300

    created interests class

---
+++ DAL/Interests.cs
@@ -0,0 +1,13 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Interests
+    {
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string fk_names_guid { get; set; }
+        public string interest { get; set; }
+        public string interestother { get; set; }
+        public string interestcategory { get; set; }
+    }
+}
