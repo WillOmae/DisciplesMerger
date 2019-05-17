8facaae66fcbb26d5c39307386a733e0052993bd
willomae <wilburomae@gmail.com>
2019-05-17 11:52:15 +0300

    created names class

---
+++ DAL/Names.cs
@@ -0,0 +1,55 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Names
+    {
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string fk_churches_guid { get; set; }
+        public string fk_workers_guid { get; set; }
+        public string fk_workers_guid2 { get; set; }
+        public string fk_smallgroups_guid { get; set; }
+        public string type { get; set; }
+        public string level { get; set; }
+        public string status { get; set; }
+        public string agegroup { get; set; }
+        public string marital { get; set; }
+        public string ethnicity { get; set; }
+        public string ethnicityother { get; set; }
+        public string language { get; set; }
+        public string languageother { get; set; }
+        public string religion { get; set; }
+        public string religionother { get; set; }
+        public string homechurch { get; set; }
+        public string attendingchurch { get; set; }
+        public string baptismdate { get; set; }
+        public string leadsource { get; set; }
+        public string firstcontact { get; set; }
+        public string title { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string street { get; set; }
+        public string unit { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postal { get; set; }
+        public string country { get; set; }
+        public string coordinates { get; set; }
+        public string phone1 { get; set; }
+        public string phone1type { get; set; }
+        public string phone2 { get; set; }
+        public string phone2type { get; set; }
+        public string email { get; set; }
+        public string gender { get; set; }
+        public string birthdate { get; set; }
+        public string referrer { get; set; }
+        public string visitday { get; set; }
+        public string visittime { get; set; }
+        public string autoschedule { get; set; }
+        public string notes { get; set; }
+        public string confidential { get; set; }
+        public string user { get; set; }
+        public string password { get; set; }
+        public string permissions { get; set; }
+        public string colorpublic { get; set; }
+    }
+}
