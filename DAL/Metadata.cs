5c8856ba552e97c5ea079a3bf2cc305fc7f93e74
willomae <wilburomae@gmail.com>
2019-05-17 11:53:37 +0300

    created metadata class

---
+++ DAL/Metadata.cs
@@ -0,0 +1,26 @@
+﻿namespace DisciplesMerger.DAL
+{
+    public class Metadata
+    {
+        public string guid { get; set; }
+        public string timestamp { get; set; }
+        public string version { get; set; }
+        public string shared { get; set; }
+        public string account_id { get; set; }
+        public string lastsync { get; set; }
+        public string needssync { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string street { get; set; }
+        public string unit { get; set; }
+        public string city { get; set; }
+        public string state { get; set; }
+        public string postal { get; set; }
+        public string country { get; set; }
+        public string coordinates { get; set; }
+        public string phone1 { get; set; }
+        public string phone2 { get; set; }
+        public string email { get; set; }
+        public string notes { get; set; }
+    }
+}
