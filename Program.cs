8ce0ce085010af64e56303cede2524d189a8c741
willomae <wilburomae@gmail.com>
2019-05-24 10:31:12 +0300

    moved some sections to their own methods;

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DisciplesMerger
 {
@@ -10,8 +7,26 @@ namespace DisciplesMerger
     {
         static void Main(string[] args)
         {
-            var NEWLINE = Environment.NewLine;
-            var DOUBLENEWLINE = NEWLINE + NEWLINE;
+            var files = GetFiles();
+            switch (GetUserConsent())
+            {
+                case true:
+                    string[] filenames = new string[files.Length];
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        filenames[i] = files[i].FullName;
+                    }
+                    var disciplesMerger = new DisciplesMerger(filenames);
+                    disciplesMerger.Merge();
+                    Console.WriteLine("Done");
+                    break;
+                case false:
+                    break;
+            }
+            Console.ReadKey();
+        }
+        private static FileInfo[] GetFiles()
+        {
             var launchPath = Environment.CurrentDirectory;
             var databasesPath = Path.Combine(launchPath, "Databases");
             var databaseFolder = Directory.CreateDirectory(databasesPath);
@@ -23,29 +38,18 @@ namespace DisciplesMerger
             {
                 Console.WriteLine("{0}. " + files[i], i + 1);
             }
-            string response = string.Empty;
+            return files;
+        }
+        private static bool GetUserConsent()
+        {
+            string response;
             do
             {
-                Console.WriteLine("Are they all databases? Yes(y) to proceed, No (n) to cancel.");
+                Console.WriteLine("Are they all databases? Yes(y) to proceed, No (n) to cancel or exit.");
                 var input = Console.ReadLine();
-                response = input.ToLower().Contains('y') ? "yes" : input.ToLower().Contains('n') ? "exit" : "invalid";
+                response = input.ToLower().Contains("y") ? "yes" : input.ToLower().Contains("n") ? "exit" : "invalid";
             } while (!response.Equals("yes") && !response.Equals("exit"));
-
-            switch (response)
-            {
-                case "yes":
-                    string[] filenames = new string[files.Length];
-                    for (int i = 0; i < files.Length; i++)
-                    {
-                        filenames[i] = files[i].FullName;
-                    }
-                    var disciplesMerger = new DisciplesMerger(filenames);
-                    disciplesMerger.Merge();
-                    break;
-                case "exit":
-                    break;
-            }
-            Console.ReadKey();
+            return response.Equals("yes") ? true : false;
         }
     }
 }
